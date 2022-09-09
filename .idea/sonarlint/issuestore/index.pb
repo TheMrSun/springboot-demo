7214127a73f3b60f0ae192fc6cc1ca08c1b17894
Slience <2890554485@qq.com>
2022-09-09 14:41:40 +0800

    1. springBoot-demo 名称更改

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+U
+%springboot-demo-properties/.gitignore,4\1\41816e180cfde05d43dad4683e114a156013b318
+€
+Pspringboot-demo-properties/src/test/java/com/sunz/demo/DemoApplicationTests.java,f\2\f2ea9fdc4b532ba489c7274d8d325f8b96183b1e
--- springboot-demo-properties/src/main/java/com/sunz/demo/DemoPropertiesApplication.java
+++ springboot-demo-properties/src/main/java/com/sunz/demo/DemoPropertiesApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoPropertiesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoPropertiesApplication.class, args);
     }
+
 }
